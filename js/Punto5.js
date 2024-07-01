25a5fd213579a293d7954c4127af69e2e12b083b
Gabriel Haurane <gabrielhaurane@gmail.com>
2024-07-01 11:46:22 -0300

    ajuste del repo

---
+++ js/Punto5.js
@@ -0,0 +1,8 @@
+/*5.- Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+
+Nota: Tener en cuenta la siguiente función: parseInt
+*/
+
+const num1 = prompt("ingrese el primer numero");
+const num2 = prompt("ingrese el segundo numero");
+alert("la suma es: " + (parseInt(num1) + parseInt(num2)));
