a6d25e4fb0066888033342bf9b172c7e0f0f526d
Gabriel Haurane <gabrielhaurane@gmail.com>
2024-07-02 19:04:16 -0300

    arreglo del punto 9 y tp terminado

--- js/Punto9.js
+++ js/Punto9.js
@@ -7,12 +7,26 @@ input: Hola mundo
 Output: oauo
 
 */
-const frase = prompt(`agrege una frase hasta 10 caracteres`)
+const frase = prompt(`agrege una frase hasta 5 caracteres`)
+let fraseVocales ="";
 
-if (frase.length <= 10 ) {
-    if (frase.substring(0,10) == `a` || frase.substring(0,10) == `e` || frase.substring(0,10) == `i`|| frase.substring(0,10) == `o` || frase.substring(0,10) == `u`) {
-        console.log(`sus vocales son ${frase}`);
+if (frase.length <= 5 ) {
+    if (frase[0] == `a` || frase[1] == `a` || frase[2] == `a` || frase[3] == `a` || frase[4] == `a` || frase[5] == `a`) {
+        fraseVocales += `a`
     }
+    if (frase[0] == `e` || frase[1] == `e` || frase[2] == `e` || frase[3] == `e` || frase[4] == `e` || frase[5] == `e`) {
+        fraseVocales += `e`
+    }
+    if (frase[0] == `i` || frase[1] == `i` || frase[2] == `i` || frase[3] == `i` || frase[4] == `i` || frase[5] == `i`) {
+        fraseVocales += `i`
+    }
+    if (frase[0] == `o` || frase[1] == `o` || frase[2] == `o` || frase[3] == `o` || frase[4] == `o` || frase[5] == `o`) {
+        fraseVocales += `o`
+    }
+    if (frase[0] == `u` || frase[1] == `u` || frase[2] == `u` || frase[3] == `u` || frase[4] == `u` || frase[5] == `u`) {
+        fraseVocales += `u`
+    }
+    document.write(fraseVocales)
     
 } else {
     document.write(`la cantidad de caracteres es incorrecta`)
